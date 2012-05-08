acce1594de05e1c07e247512a4e9cacd00d21776
Zach Bruhnke <z@zbruhnke.com>
2012-05-08 09:13:57 -0700

    save from webhook

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -4,6 +4,8 @@ require 'json'
   def create
     Rails.logger.info "I got some JSON: #{params}"
     data = JSON.parse(request.body.read)
+    @webhook.object = params[:webhook]
+    @webhook.save
     render nothing: true, status: 200
   end
 end

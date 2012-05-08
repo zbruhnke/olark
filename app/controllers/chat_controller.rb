7e169ec2979947c4eea3a2e9c4f45824e3c4d83a
Zach Bruhnke <z@zbruhnke.com>
2012-05-08 10:55:37 -0700

    redirect

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -5,6 +5,14 @@ require 'json'
     Rails.logger.info "I got some JSON: #{params}"
     @chat = Chat.new(params[:kind])
     render nothing: true, status: 200
-    print 'it worked!'
+    respond_to do |format|
+      if @chat.save
+        format.html { redirect_to(@chat, :notice => 'Chat was successfully saved.') }
+        format.xml  { render :xml => @chat, :status => :created, :location => @chat }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @chat.errors, :status => :unprocessable_entity }
+      end
+    end
   end
 end

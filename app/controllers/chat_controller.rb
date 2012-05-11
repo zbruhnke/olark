4ceb5097064270489f1a6156c6e4727c8c97fe8a
Zach Bruhnke <z@zbruhnke.com>
2012-05-11 15:26:24 -0700

    opne more time

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -2,7 +2,29 @@ class ChatController < ApplicationController
 require 'json'
 
   def create
-    @chat = JSON.parse(params[:browser, :city, :country, :country_code, :email_address, :fullname, :id, :ip, :kind, :operating_system, :organization, :region])
+    @chat = JSON.parse({
+      kind: 'conversation',
+      id: 'EV695BI2930A6XMO32886MPT899443414',
+      items: [
+        {kind: 'MessageToVisitor', nickname: "John", timestamp: "1307116657.1", body: "Hi there. Need any help?"},
+        {kind: 'MessageToOperator', nickname: "Bob", timestamp: "1307116661.25", body: "Yes, please help me with billing."}
+      ],
+      visitor: {
+        kind: 'Visitor',
+        id: '9QRF9YWM5XW3ZSU7P9CGWRU89944341',
+        fullName: "John Doe",
+        emailAddress: "foo@example.com",
+        city: "Palo Alto",
+        region: "CA",
+        country: "United State",
+        countryCode: "US",
+        organization: "Widgets Inc.",
+        ip: "123.4.56.78",
+        browser: "Chrome 12.1",
+        operatingSystem: "Windows"
+        customFields: {myInternalCustomerId: "12341234", favoriteColor: "blue"}
+      }
+    })
     
     respond_to do |format|
       if @chat.save

7c5df6ff04e983411dda6e5b719bb7064b390383
Zach Bruhnke <z@zbruhnke.com>
2012-05-11 09:45:08 -0700

    accept hash

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -3,10 +3,11 @@ require 'json'
 
   def create
     Rails.logger.info "I got some JSON: #{params}"
-    @chat = Chat.new.from_json(params[:chat].to_json)
-    #@chat = Chat.new(:name => params[:chat][:name])
+    @chat = Chat.new({:browser => params[:broswer], :city => params[:city], :country => params[:country], :fullname => params[:fullname], :id => params[:id], :ip => params[:ip], :kind => params[:kind], :operating_system => params[:operating_system], :organization => params[:organization], :region => params[:region] })
+
     respond_to do |format|
       if @chat.save
+        flash[:notice] = 'Chat was successfully created.'
         render nothing: true, status: 200
       else
         format.json  { render json: @chat.errors, status: :unprocessable_entity }

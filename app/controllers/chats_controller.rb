d0abe3186c59a3de95e2d337182a8353c8343af8
Zach Bruhnke <z@zbruhnke.com>
2012-05-13 22:29:10 -0700

    skip before filer chats

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,5 @@
 class ChatsController < ApplicationController
+skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
 require 'json'
 
   def create

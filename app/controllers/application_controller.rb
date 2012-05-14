78eaa65a4d4108573deab36cddf391e3d56a9698
Zach Bruhnke <z@zbruhnke.com>
2012-05-13 22:31:48 -0700

    remove protect from forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,2 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-  skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
 end
--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,5 +1,4 @@
 class ChatsController < ApplicationController
-skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
 require 'json'
 
   def create

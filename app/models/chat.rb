13bb251e31d335eae8d5a6dd7a615fa93f36dad1
Zach Bruhnke <z@zbruhnke.com>
2012-05-14 15:50:47 -0700

    nest correctly

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,20 +2,21 @@ class Chat < ActiveRecord::Base
   attr_accessible :browser, :city, :country, :country_code, :email_address, :fullname, :olark_id, :visitor_id, :ip, :kind, :operating_system, :organization, :region, :data
 
   def self.new_from_json(params)
+    params[:visitor][:item_count] = params[:items].size	
+    params[:items] = nil
     new({
-          kind: params[:kind],
-          visitor_id: params[:visitor_id],
-          olark_id: params[:olark_id],
-          fullname: params['fullName'],
-          email_address: params['emailAddress'],
-          city: params[:city],
-          region: params[:region],
-          country: params[:country],
-          country_code: params['countryCode'],
-          organization: params[:organization],
-          ip: params[:ip],
-          browser: params[:browser],
-          operating_system: params['operatingSystem'],
+          kind: params[:visitor][:kind],
+          visitor_id: params[:visitor][:visitor_id],
+          fullname: params[:visitor]['fullName'],
+          email_address: params[:visitor]['emailAddress'],
+          city: params[:visitor][:city],
+          region: params[:visitor][:region],
+          country: params[:visitor][:country],
+          country_code: params[:visitor]['countryCode'],
+          organization: params[:visitor][:organization],
+          ip: params[:visitor][:ip],
+          browser: params[:visitor][:browser],
+          operating_system: params[:visitor]['operatingSystem'],
         })
   end
 end

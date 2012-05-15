0a71a4f8c24c2351f1d9db5af74e2816a2811dba
Zach Bruhnke <z@zbruhnke.com>
2012-05-14 17:22:06 -0700

    revert

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,21 +1,21 @@
 class Chat < ActiveRecord::Base
   attr_accessible :browser, :city, :country, :country_code, :email_address, :fullname, :olark_id, :visitor_id, :ip, :kind, :operating_system, :organization, :region, :data
-  has_many :visitor
-     accepts_nested_attributes_for :visitor, :allow_destroy => true
+
   def self.new_from_json(params)
     new({
-          kind: params[:visitor][:kind],
-          visitor_id: params[:visitor][:visitor_id],
-          fullname: params[:visitor]['fullName'],
-          email_address: params[:visitor]['emailAddress'],
-          city: params[:visitor][:city],
-          region: params[:visitor][:region],
-          country: params[:visitor][:country],
-          country_code: params[:visitor]['countryCode'],
-          organization: params[:visitor][:organization],
-          ip: params[:visitor][:ip],
-          browser: params[:visitor][:browser],
-          operating_system: params[:visitor]['operatingSystem'],
+          kind: params[:kind],
+          visitor_id: params[:visitor_id],
+          olark_id: params[:olark_id],
+          fullname: params['fullName'],
+          email_address: params['emailAddress'],
+          city: params[:city],
+          region: params[:region],
+          country: params[:country],
+          country_code: params['countryCode'],
+          organization: params[:organization],
+          ip: params[:ip],
+          browser: params[:browser],
+          operating_system: params['operatingSystem'],
         })
   end
 end
